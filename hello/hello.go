135d2939f11f4f2e1651cfc876bfb6be233b04e5
jackschu <js9pa@virginia.edu>
2019-03-13 20:11:59 -0400

    adding documentation ;)

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,13 @@
+// A program to print "hello world"
+// followed by a newline
 package main
 
 import "fmt"
 
-func main() {
+func sayHello() {
 	fmt.Printf("hello world\n")
+}
+
+func main() {
+	sayHello()
 }
