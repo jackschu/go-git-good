42a0f7b80c80030dd154abcc7c0f6cea3d3dfea7
jackschu <js9pa@virginia.edu>
2019-03-14 23:55:09 -0400

    getting valid decimals from numbers works

--- leetcode/ambiguous-coordinates/ambiguous-coordinates.go
+++ leetcode/ambiguous-coordinates/ambiguous-coordinates.go
@@ -8,9 +8,77 @@ import (
 	"strings"
 )
 
+func insert(i int, val rune, slice []rune) []rune {
+	slice = append(slice, rune(0))
+	copy(slice[i+1:], slice[i:])
+	slice[i] = val
+	return slice
+}
+
+func isNoDecimalLegal(list []rune) bool{
+	if len(list) == 1 && list[0] == '0' {
+		return true
+	}
+	ct := 0
+	for _, r := range list{
+		if ct == 2 {
+			return false
+		}
+		
+		if r != '0' {
+			return true
+		} else {
+			ct++ 
+		}
+	}
+	return false
+}
+
+func isValidDecimalPart(list []rune) bool {
+	if len(list) == 0 {
+		return false
+	}
+	if list[len(list)-1] == '0' {
+		return false
+	}
+	return true
+}
+
+func validDecimals(list []rune) []string {
+	var out []string
+	if isNoDecimalLegal(list) {
+		temp := make([]rune, len(list))
+		copy(temp, list)
+		out = append(out, string(temp))
+	}
+
+	for i := 1; i < len(list); i++ {
+		front := list[:i]
+		back := list[i:]
+		if isNoDecimalLegal(front) && isValidDecimalPart(back){
+			out = append(out, string(front) + "." + string(back))
+		}
+		
+	}
+	
+	return out 
+}
+
 func ambiguousCoordinates(S string) []string {
-	r := strings.Split(strings.Trim(S,"()"),"")
-	fmt.Println(r)
+	list := []rune(strings.Trim(S, "()"))
+//	var out []string
+	fmt.Println(validDecimals(list))
+/*	//put comma first
+	for i := 1; i < len(list); i++ {
+		for first := 1; first < i; first++ {
+			for second := i; second < len(list); second++ {
+			
+			}
+		}
+	}
+
+	
+	fmt.Println(out)*/
 	return []string{"hi"}
 }
 
@@ -27,7 +95,5 @@ func main() {
 	s = sc.Text()
 	fmt.Println(ambiguousCoordinates(s))
 }
-
-
 
 //  LocalWords:  ambiguousCoordinates AmbiguousCoordinates
